bfb20aec033bb5959223dae0dfec00868472372c
Matt Poland <matthew.poland1@gmail.com>
2012-07-05 19:44:38 -0400

    minor changes

---
+++ email.js
@@ -0,0 +1,13 @@
+<!-- script via http://www.designplace.org/scripts.php?page=1&c_id=20 -->
+<script language="Javascript" type="text/javascript">
+  <!-- // hide from old browsers
+
+  //var
+  var user = "matt";
+  var domain = "mattpoland.net";
+
+  //output
+  document.write('<a href=\"mailto:' + user + '@' + domain + '\">'); 
+  document.write(<li><i class="icon-envelope"></i></li></a>);
+ // -->
+ </script>
